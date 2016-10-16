196e70c4f723fa468665dc02d096fe5e37408e76
Franziska Schmidt <franziska.petra@gmail.com>
2016-10-16 18:48:35 +0200

    Add figwheel

--- project.clj
+++ project.clj
@@ -6,12 +6,13 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [quil "2.4.0"]
                  [org.clojure/clojurescript "1.8.34"]]
-
-  :plugins [[lein-cljsbuild "1.1.3"]]
+  :plugins [[lein-cljsbuild "1.1.3"]
+            [lein-figwheel "0.5.8"]]
   :hooks [leiningen.cljsbuild]
-
   :cljsbuild
   {:builds [{:source-paths ["src"]
+             :id "dev"
+             :figwheel true
              :compiler
              {:output-to "js/main.js"
               :output-dir "out"
